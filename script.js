fa6f17b6dcffdeb1e71c28054cfce09f94543eac
leticiaandrade <leticia933@gmail.com>
2021-11-25 01:53:58 -0300

    requisito 10

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
-const criarTarefa = document.getElementById('criar-tarefa')
+const criarTarefa = document.getElementById('criar-tarefa');
+const apagarTudo = document.getElementById('apaga-tudo');
 //console.log(mudarCorTarefa);
 //console.log(criarTarefa)
 criarTarefa.addEventListener('click', getTextInput);
+apagarTudo.addEventListener('click', clearAll);
 
 function getTextInput() {
     //criar uma variavel que guarde o valor do texto
@@ -31,7 +33,8 @@ function getTextInput() {
 let listaPronta = document.querySelector('#lista-tarefas').children;
 console.log(listaPronta)
 
-
+//ref: https://developer.mozilla.org/pt-BR/docs/Web/API/Node/contains
+//https://www.w3schools.com/jsref/met_element_remove.asp
 function changeColor(event) {
     //criar variavel que guarde os itens que o usuario adicionou à lista
     let listaPronta = document.querySelector('#lista-tarefas').children;
@@ -48,6 +51,15 @@ function changeColor(event) {
     event.target.classList.add('selected-item')
     //adicionar a classe selected sem remover a anterior
     //event.target.classList.add('selected-item')     
+}
+
+//https://stackoverflow.com/questions/67769790/how-do-i-delete-list-items-with-clear-button-javascript
+function clearAll() {
+    /* let listaToda = document.getElementById('lista-tarefas').children;
+    for (let c = 0; c < listaToda.length; c += 1) {
+        listaToda[c].remove();
+    } */
+    document.getElementById('lista-tarefas').innerHTML = '';
 }
 
 /* if (itemSelected !== null) {
@@ -68,3 +80,4 @@ function changeColor(event) {
 }   
 
 console.log(mudaCor) */
+
